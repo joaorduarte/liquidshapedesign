5121a1b5b878025fc64a935e5a4b035e30330d4a
joaorduarte <joaorduarte@gmail.com>
2020-05-20 14:44:46 +0100

    Updates

---
+++ precache-manifest.88d66f68df3dcc63599c7bdccb59e29e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bbc8b74685105f839503fb475ee53fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "040cd830141fe3db50b6",
+    "url": "/static/css/main.af5dfcf4.chunk.css"
+  },
+  {
+    "revision": "3a5b04eb0167d9a32da5",
+    "url": "/static/js/2.e4ea8064.chunk.js"
+  },
+  {
+    "revision": "a2bea5a6194516414a5f76919ab82893",
+    "url": "/static/js/2.e4ea8064.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "040cd830141fe3db50b6",
+    "url": "/static/js/main.eae8f148.chunk.js"
+  },
+  {
+    "revision": "fa432c6f350557b78118",
+    "url": "/static/js/runtime-main.19c17167.js"
+  },
+  {
+    "revision": "804306f3626fbe5390e46b81b23ca51d",
+    "url": "/static/media/Dev Flow Horz.804306f3.png"
+  },
+  {
+    "revision": "9e9386323d338ef9ceae5b67435c3e64",
+    "url": "/static/media/Dev Flow.9e938632.png"
+  },
+  {
+    "revision": "27bda4d4d1a9f670128f25edc4903069",
+    "url": "/static/media/Flow Listing.27bda4d4.png"
+  },
+  {
+    "revision": "9b70d11aaeed1e574ed10c300abed5ec",
+    "url": "/static/media/Flow Map.9b70d11a.png"
+  },
+  {
+    "revision": "72f388da03300e955098c50fe22350c7",
+    "url": "/static/media/FlowBasic.72f388da.png"
+  },
+  {
+    "revision": "b3ff1afd0f924e914c3382b095c1a701",
+    "url": "/static/media/Goals.b3ff1afd.png"
+  },
+  {
+    "revision": "087257e0cd22e6d41dcdf0581e93c042",
+    "url": "/static/media/HypothesesFluid.087257e0.png"
+  },
+  {
+    "revision": "4fa4639a0ddf76ba1619f5cccf67a0ca",
+    "url": "/static/media/HypothesesStructured.4fa4639a.png"
+  },
+  {
+    "revision": "6c383c86296e5d994432144d49b3f4ca",
+    "url": "/static/media/Sq1.6c383c86.svg"
+  },
+  {
+    "revision": "8a72a1204b745b306bc1937d55baa687",
+    "url": "/static/media/Sq2.8a72a120.svg"
+  },
+  {
+    "revision": "50085bca7f24a89c9aff53398a44bb36",
+    "url": "/static/media/Sq3.50085bca.svg"
+  },
+  {
+    "revision": "878c654d51feacba43f547784a69858f",
+    "url": "/static/media/User Flow.878c654d.png"
+  },
+  {
+    "revision": "c529f6f5feb1455ce73fc1cecef19f5a",
+    "url": "/static/media/User Stories.c529f6f5.png"
+  },
+  {
+    "revision": "b1e6124840f2789401ff6054a31885f4",
+    "url": "/static/media/made-design-showroom.b1e61248.jpg"
+  },
+  {
+    "revision": "dc4b7f335f7dd3935622a6fcc179bc7c",
+    "url": "/static/media/mindmeister.dc4b7f33.png"
+  }
+]);
